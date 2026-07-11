cbfbe9edc5c7840268d67d157d4695d3ae3b2024
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-12 01:13:02 +1200

    Update PresenceHubApp.kt

--- app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
+++ app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import com.ombryal.presencehub.navigation.AppNavigation
 import com.ombryal.presencehub.plugins.PluginRegistryEntry
 import com.ombryal.presencehub.plugins.PluginStoreState
+import com.ombryal.presencehub.ui.account.AccountUiState
 
 @Composable
 fun PresenceHubApp(
     storeState: PluginStoreState,
+    accountState: AccountUiState,
     onRefreshPlugins: () -> Unit,
     onInstallPlugin: (PluginRegistryEntry) -> Unit,
     onUninstallPlugin: (PluginRegistryEntry) -> Unit,
@@ -16,6 +18,7 @@ fun PresenceHubApp(
 ) {
     AppNavigation(
         storeState = storeState,
+        accountState = accountState,
         onRefreshPlugins = onRefreshPlugins,
         onInstallPlugin = onInstallPlugin,
         onUninstallPlugin = onUninstallPlugin,
