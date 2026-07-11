0a19c5ca8922d8d892e7e96d25f195a676f69fa8
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-12 00:36:28 +1200

    Update AppNavigation.kt

--- app/src/main/java/com/ombryal/presencehub/navigation/AppNavigation.kt
+++ app/src/main/java/com/ombryal/presencehub/navigation/AppNavigation.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
