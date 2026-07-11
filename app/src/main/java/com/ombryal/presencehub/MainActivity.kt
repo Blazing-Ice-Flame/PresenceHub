1ec0d37f8723d234a65908f043ed2d3883ca4190
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-11 20:52:27 +1200

    Update MainActivity.kt

--- app/src/main/java/com/ombryal/presencehub/MainActivity.kt
+++ app/src/main/java/com/ombryal/presencehub/MainActivity.kt
@@ -3,12 +3,11 @@ package com.ombryal.presencehub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.ombryal.presencehub.plugins.InstalledPluginRegistry
 import com.ombryal.presencehub.plugins.PluginInstallManager
-import com.ombryal.presencehub.plugins.PluginRegistryEntry
 import com.ombryal.presencehub.plugins.PluginStore
 import com.ombryal.presencehub.plugins.PluginStoreState
 import com.ombryal.presencehub.service.ServiceController
