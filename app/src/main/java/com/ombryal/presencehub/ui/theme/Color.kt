380ac037d2c658ffd999c7fdbf16faf3e1516b3e
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-12 15:14:30 +1200

    Update Color.kt

--- app/src/main/java/com/ombryal/presencehub/ui/theme/Color.kt
+++ app/src/main/java/com/ombryal/presencehub/ui/theme/Color.kt
@@ -2,12 +2,17 @@ package com.ombryal.presencehub.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val GradientTop = Color(0xFF0D0D12)
-val GradientBottom = Color(0xFF16141B)
+val GradientTop = Color(0xFF09090F)
+val GradientBottom = Color(0xFF151522)
 
 val PurpleGlow = Color(0xFF8B5CF6)
+val VioletGlow = Color(0xFFA855F7)
 val IndigoGlow = Color(0xFF6366F1)
-val PinkGlow = Color(0xFFC084FC)
-val GrayPurpleGlow = Color(0xFFA78BFA)
+val BlueGlow = Color(0xFF3B82F6)
+val CyanGlow = Color(0xFF22D3EE)
+val TealGlow = Color(0xFF14B8A6)
+val PinkGlow = Color(0xFFF472B6)
+val MagentaGlow = Color(0xFFEC4899)
+val AmberGlow = Color(0xFFF59E0B)
 
 val VignetteColor = Color(0x33000000)
