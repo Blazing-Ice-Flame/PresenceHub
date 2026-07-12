28e7c40c864627d47e6cef9176714f568c81fd61
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-12 15:14:59 +1200

    Update Background.kt

--- app/src/main/java/com/ombryal/presencehub/ui/theme/Background.kt
+++ app/src/main/java/com/ombryal/presencehub/ui/theme/Background.kt
@@ -9,21 +9,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 
-val GradientTop = Color(0xFF09090F)
-val GradientBottom = Color(0xFF151522)
-
-val PurpleGlow = Color(0xFF8B5CF6)
-val VioletGlow = Color(0xFFA855F7)
-val IndigoGlow = Color(0xFF6366F1)
-val BlueGlow = Color(0xFF3B82F6)
-val CyanGlow = Color(0xFF22D3EE)
-val TealGlow = Color(0xFF14B8A6)
-val PinkGlow = Color(0xFFF472B6)
-val MagentaGlow = Color(0xFFEC4899)
-val AmberGlow = Color(0xFFF59E0B)
-
-val VignetteColor = Color(0x33000000)
-
 @Composable
 fun GlassmorphismBackground(modifier: Modifier = Modifier) {
     Canvas(modifier = modifier.fillMaxSize()) {
