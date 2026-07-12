2b09e1a99f4c3e78cdf4e10395050edfe02a58f0
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-12 12:50:26 +1200

    Update PresenceHubApp.kt

--- app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
+++ app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
@@ -5,24 +5,31 @@ import com.ombryal.presencehub.navigation.AppNavigation
 import com.ombryal.presencehub.plugins.PluginRegistryEntry
 import com.ombryal.presencehub.plugins.PluginStoreState
 import com.ombryal.presencehub.ui.account.AccountUiState
+import com.ombryal.presencehub.ui.settings.SettingsUiState
 
 @Composable
 fun PresenceHubApp(
     storeState: PluginStoreState,
     accountState: AccountUiState,
+    settingsState: SettingsUiState,
     onRefreshPlugins: () -> Unit,
     onInstallPlugin: (PluginRegistryEntry) -> Unit,
     onUninstallPlugin: (PluginRegistryEntry) -> Unit,
     onStartRpc: () -> Unit,
-    onStopRpc: () -> Unit
+    onStopRpc: () -> Unit,
+    onOpenSettings: () -> Unit,
+    onUpdateSettings: (SettingsUiState) -> Unit
 ) {
     AppNavigation(
         storeState = storeState,
         accountState = accountState,
+        settingsState = settingsState,
         onRefreshPlugins = onRefreshPlugins,
         onInstallPlugin = onInstallPlugin,
         onUninstallPlugin = onUninstallPlugin,
         onStartRpc = onStartRpc,
-        onStopRpc = onStopRpc
+        onStopRpc = onStopRpc,
+        onOpenSettings = onOpenSettings,
+        onUpdateSettings = onUpdateSettings
     )
 }
