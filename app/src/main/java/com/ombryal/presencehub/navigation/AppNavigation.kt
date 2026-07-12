693c3b977c5459d101dcbe94a318d4bb05825578
Blazing-Ice-Flame <151167310+Blazing-Ice-Flame@users.noreply.github.com>
2026-07-12 14:31:24 +1200

    Update AppNavigation.kt

--- app/src/main/java/com/ombryal/presencehub/navigation/AppNavigation.kt
+++ app/src/main/java/com/ombryal/presencehub/navigation/AppNavigation.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -78,6 +79,7 @@ fun AppNavigation(
     var selectedPlugin by remember { mutableStateOf<PluginRegistryEntry?>(null) }
 
     Scaffold(
+        containerColor = Color.Transparent,
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
